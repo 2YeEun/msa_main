8d06eb01e4ca17488058c316cfabdedb5ff148a5
2YeEun <37912328+2YeEun@users.noreply.github.com>
2024-12-16 13:27:01 +0900

    Update MsaMainApplicatio1n.java

--- src/main/java/hana/corp/msa_main/MsaMainApplication.java
+++ src/main/java/hana/corp/msa_main/MsaMainApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsaMainApplication {
-
-
     
     public static void main(String[] args) {
         SpringApplication.run(MsaMainApplication.class, args);
